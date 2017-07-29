e4c3eb5b59acc05da353356cf27de5e697515b6c
Ishiwawa <arnaud_lecomte@outlook.com>
2017-07-29 17:12:06 +0200

    PdVHéros

---
+++ Wizard Dungeon/Assets/Evil.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evil : MonoBehaviour {
+
+	// Use this for initialization
+
+
+
+		void OnTriggerEnter2D(Collider2D co) {
+		if (co.name == "sword")
+				Destroy(gameObject);
+		}
+
+}
---
+++ Wizard Dungeon/Assets/LifeJ.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeJ : MonoBehaviour {
+	static int lifemax = 2;
+	public int life = lifemax;
+
+	void Update(){
+		if (life == 0) {
+			Destroy (gameObject);
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D co) {
+		if (co.name == "zou_0")
+			life -= 1;
+	}
+		
+}
