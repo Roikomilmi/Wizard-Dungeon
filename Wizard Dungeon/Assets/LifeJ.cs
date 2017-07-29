682cb2315ffe4e4cc83d98612dd3fccbe8be6270
Ishiwawa <arnaud_lecomte@outlook.com>
2017-07-29 17:20:06 +0200

    Invincible

--- Wizard Dungeon/Assets/LifeJ.cs
+++ Wizard Dungeon/Assets/LifeJ.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class LifeJ : MonoBehaviour {
 	static int lifemax = 2;
 	public int life = lifemax;
+	bool invincible = false;
+	public int TempsInv;
 
 	void Update(){
 		if (life == 0) {
@@ -14,8 +16,17 @@ public class LifeJ : MonoBehaviour {
 
 
 	void OnTriggerEnter2D(Collider2D co) {
-		if (co.name == "zou_0")
-			life -= 1;
+		if (!invincible) {
+			if (co.name == "zou_0") {
+				life -= 1;
+				StartCoroutine (Invincibilite ());
+			}
+		}
 	}
-		
+	public IEnumerator Invincibilite(){
+		invincible = true;
+		Debug.Log ("bla");
+		yield return new WaitForSeconds (TempsInv);
+		invincible = false;
+	}
 }
