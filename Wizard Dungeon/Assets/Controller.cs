60dd3b0c44c15eac8d738fe03277679d4a7a40b2
Roikomilmi <roikomilmi@gmail.com>
2017-07-30 04:38:52 +0200

    Animation Link ok

--- Wizard Dungeon/Assets/Controller.cs
+++ Wizard Dungeon/Assets/Controller.cs
@@ -3,49 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour {
-	bool check= false;
-
 	public float speed = 2f;
+	Vector2 dest = Vector2.zero;
 
 	// Use this for initialization
 	void Start () {
+		dest = transform.position;
 	}
 
 	// Update is called once per frame
-	void Update () {
-		CheckKeys();
-
+	void FixedUpdate () {
+		dest = transform.position;
+		if ((Vector2)transform.position == dest) {
+			CheckKeys ();
+		}
+		Vector2 dir = dest - (Vector2)transform.position;
+		GetComponent<Animator>().SetFloat("DirX", dir.x);
+		GetComponent<Animator>().SetFloat("DirY", dir.y);
+		Debug.Log("x "+dir.x+" y "+dir.y);
 	}
 
 	void CheckKeys(){
-		Vector2 Truc = (Vector2)transform.position;
-		if(Input.GetKey(KeyCode.Q)){
-			Truc += Vector2.left * speed * Time.deltaTime;
-			transform.localPosition = Truc;
+		Vector2 posJ = (Vector2)transform.position;
+		if(Input.GetKey(KeyCode.Q) && valid(-Vector2.right)){
+			Debug.Log("CUL !");
+			posJ += Vector2.left * speed * Time.deltaTime;
+			transform.localPosition = posJ;
 		}
-		if(Input.GetKey(KeyCode.D)){
-			Truc += Vector2.right * speed * Time.deltaTime;
-			transform.localPosition = Truc;
+		if(Input.GetKey(KeyCode.D) && valid(Vector2.right)){
+			posJ += Vector2.right * speed * Time.deltaTime;
+			transform.localPosition = posJ;
 		}
-		if(Input.GetKey(KeyCode.Z)){
-			Truc += Vector2.up * speed * Time.deltaTime;
-			transform.localPosition = Truc;
+		if(Input.GetKey(KeyCode.Z) && valid(Vector2.up)){
+			posJ += Vector2.up * speed * Time.deltaTime;
+			transform.localPosition = posJ;
 		}
-		if(Input.GetKey(KeyCode.S)){
-			Truc += Vector2.down * speed * Time.deltaTime;
-			transform.localPosition = Truc;
+		if (Input.GetKey (KeyCode.S) && valid (-Vector2.up)) {
+			posJ += Vector2.down * speed * Time.deltaTime;
+			transform.localPosition = posJ;
+		} else {
 		}
 
-
-}
-
-void OnCollisionEnter(Collision col){
-	if(col.collider.gameObject.layer==LayerMask.NameToLayer("Coffre")){
-		//CheckKeys();
-		if(/*check == true*/ Input.GetKey(KeyCode.Mouse0)){
-			Debug.Log ("Coffre");
-		}
-		//afficher dialogue de liste coffre
 	}
-}
+
+
+	bool valid(Vector2 dir) {
+		// Cast Line from 'next to Pac-Man' to 'Pac-Man'
+		Vector2 pos = (Vector2)transform.position;
+		RaycastHit2D hit = Physics2D.Linecast((pos + dir), pos);
+		return (hit.collider == GetComponent<Collider2D>());
+	}
 }
