4e548d2b2b1e1c7157435a74dd6e9057f5d67f14
Ishiwawa <arnaud_lecomte@outlook.com>
2017-07-30 22:16:24 +0200

    Spawner

---
+++ Wizard Dungeon/Assets/SpawnEnnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnnemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public Transform ennemi;
+	Vector2 Position= new Vector2();
+	bool Timer = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (!Timer){
+			StartCoroutine( Delai());
+			Position = new Vector2(Random.Range(-8.30f , 8.30f) , Random.Range(-4.50f, 4.50f));
+			Instantiate(ennemi, Position, transform.rotation);
+		}
+	}
+
+	public IEnumerator Delai(){	
+		
+		Timer = true;
+		yield return new WaitForSeconds (5);
+		Timer = false;
+	}
+
+}
