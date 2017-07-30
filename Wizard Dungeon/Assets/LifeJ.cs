16580df6980d4b749f93c7609bd61d8506ce78eb
Ishiwawa <arnaud_lecomte@outlook.com>
2017-07-30 04:44:09 +0200

    ReculPersonnage

--- Wizard Dungeon/Assets/LifeJ.cs
+++ Wizard Dungeon/Assets/LifeJ.cs
@@ -4,87 +4,64 @@ using UnityEngine;
 
 
 public class LifeJ : MonoBehaviour {
-	static int lifemax = 2;
+	static int lifemax = 2;	//Max points de vie, peut être changé en amont
 	public int life = lifemax;
 	bool invincible = false;
-	public int TempsInv;
+	public int TempsInv;	//Temps invincibilité, peut être changé en amont
 
 
 
 
-	void Update(){
+	void Update(){			//Permet de savoir si le personnage est mort
 		if (life == 0) {
 			Destroy (gameObject);
 		}
 	}
 
 
-	void OnTriggerEnter2D(Collider2D co) {
+	void OnTriggerEnter2D(Collider2D co) {	//Permet de trigger le fait qu'un ennemi nous touche
 		if (!invincible) {
 			if (co.name == "zou_0") {
-				life -= 1;
+				life -= 1;					//Pert 1 points de vie
 				Recul();
-				StartCoroutine (Invincibilite ());
+				StartCoroutine (Invincibilite ());	//Rends invincible
 			}
 		}
 	}
 
-	public void Recul(){
-		float ennemiPosX = GameObject.Find ("zou_0").transform.position.x;
-		float ennemiPosY = GameObject.Find ("zou_0").transform.position.y;
-		float PosX = transform.position.x;
-		float PosY = transform.position.y;
+	public void Recul(){ //Permet de reculer quand des dégats sont pris
 
-		/*if (ennemiPosX > PosX) {
-			Debug.Log ("Link à gauche");
-		}
-		if (ennemiPosY > PosY) {
-			Debug.Log ("Link en bas");
-		}
-		if (PosX > ennemiPosX) {
-			Debug.Log ("Link à droite");
-		}
-		if (PosY > ennemiPosY) {
-			Debug.Log ("Link en haut");
-		}*/
-		 
-		/*if (ennemiPosX-PosX > 0) {
-			if (ennemiPosY - PosY > ennemiPosX - PosX) {
-				Debug.Log ("Link en bas");
-			} else {
-				Debug.Log ("Link à gauche");	
-			}
-		}
-		if (PosX-ennemiPosX > 0) {
-			if (ennemiPosY - PosY > ennemiPosX - PosX) {
-				Debug.Log ("Link à droite");
-			} else {
-				Debug.Log ("Link en haut");	
-			}
-		}*/
-
-		Vector2 MonstrePerso = GameObject.Find ("zou_0").transform.position - transform.position;
-		Vector2 Horizontal = new Vector2 (1, 0);
+		Vector2 MonstrePerso = GameObject.Find ("zou_0").transform.position - transform.position;	//Vecteur entre le monstre, et le perso
+		Vector2 Horizontal = new Vector2 (1, 0);													//Vecteur horizontal pour l'angle
+		Vector2 Temp = (Vector2)transform.position;													//Vecteur temporaire pour le deplacement
 
 		if ((AngleBetweenVector2 (MonstrePerso, Horizontal) > -45) && (AngleBetweenVector2 (MonstrePerso, Horizontal) < 45)) {
 			Debug.Log ("Link à gauche");
+			Temp += Vector2.left;
+			transform.localPosition = Temp;
 		} else if ((AngleBetweenVector2 (MonstrePerso, Horizontal) > 45) && (AngleBetweenVector2 (MonstrePerso, Horizontal) < 135)) {
 			Debug.Log ("Link en haut");
+		Temp += Vector2.up;
+		transform.localPosition = Temp;
 		} else if ((AngleBetweenVector2 (MonstrePerso, Horizontal) > -135) && (AngleBetweenVector2 (MonstrePerso, Horizontal) < -45)) {
 			Debug.Log ("Link en bas");
+		Temp += Vector2.down;
+		transform.localPosition = Temp;
 		} else {
 			Debug.Log ("Link à droite");
+		Temp += Vector2.right;
+		transform.localPosition = Temp;
 		}
 	}
 
-	float AngleBetweenVector2(Vector2 vec1, Vector2 vec2){
+	float AngleBetweenVector2(Vector2 vec1, Vector2 vec2){			//Permet de calculer un angle entre deux vectors
 		Vector2 vec1Rotated90 = new Vector2(-vec1.y, vec1.x);
 		float sign = (Vector2.Dot(vec1Rotated90, vec2) < 0) ? -1.0f : 1.0f;
 		return Vector2.Angle(vec1, vec2) * sign;
 	}
 
 
-	public IEnumerator Invincibilite(){
+	public IEnumerator Invincibilite(){								//Permet de rendre le personnage invincible pendant un temps donné
 		invincible = true;
 		Debug.Log ("bla");
 		yield return new WaitForSeconds (TempsInv);
